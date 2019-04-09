4ae52974e067100c45af2e27520ad7e137c237bd
jinxin0112 <m18508218948@163.com>
2019-04-09 20:09:46 +0800

    feat: remove config

--- README.md
+++ README.md
@@ -1,25 +1,42 @@
-# umi-plugin-umi-plugin-area
+# umi-plugin-area
 
-[![NPM version](https://img.shields.io/npm/v/umi-plugin-umi-plugin-area.svg?style=flat)](https://npmjs.org/package/umi-plugin-umi-plugin-area)
-[![NPM downloads](http://img.shields.io/npm/dm/umi-plugin-umi-plugin-area.svg?style=flat)](https://npmjs.org/package/umi-plugin-umi-plugin-area)
+[![NPM version](https://img.shields.io/npm/v/umi-plugin-area.svg?style=flat)](https://npmjs.org/package/umi-plugin-area)
+[![NPM downloads](http://img.shields.io/npm/dm/umi-plugin-area.svg?style=flat)](https://npmjs.org/package/umi-plugin-area)
 
+通过约定式解决 route 多版本的方案。
 
+## 何时使用
 
-## Usage
+当项目的 route 有多个版本的时候
 
-Configure in `.umirc.js`,
+## 使用
+
+1. 在`.umirc.js`中添加插件,
 
 ```js
 export default {
   plugins: [
-    ['umi-plugin-umi-plugin-area', options],
+    ['umi-plugin-area'],
   ],
 }
 ```
+2. 在根目录下的 config 目录下创建 route
+  - 如果 route 是 js 文件并返回单一路由信息则默认启动该 route 
+  - 如果 route 是文件夹， 则需要在启动命令后加入对应 route 版本的参数
+    
+    例如：
+    ```
+    ├── config
+    │   └── route
+    │       ├── ChongQing.js
+    │       └── SiChuan.js
+    └── pages
+      ├── index.css
+      └── index.js
+    ```
+    启用命令后需加入 ChongQing 或者 SiChuan
 
-## Options
 
-TODO
 
 ## LICENSE
 
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const fs = require('fs');
 const { findRoutes } = require('./utils');
 const chalk = require('chalk');
-const deeplyAssign = require('deeplyassign').default;
 
 module.exports = function(api, options) {
   const { paths, log } = api;
@@ -14,17 +13,20 @@ module.exports = function(api, options) {
     const routePath = path.resolve(paths.cwd, 'config/route');
     const routeFd = fs.statSync(routePath, { withFileTypes: true });
     if (routeFd.isDirectory()) {
-      if (findRoutes(routePath).includes(arg)) {
-        const route = require(path.resolve(routePath, arg)).default;
+      const r = findRoutes(routePath);
+      if (arg && r.includes(arg)) {
+        const route = require(path.resolve(routePath, arg)).default || [];
         api.modifyRoutes(() => {
           log.success(`已加载 ${chalk.green(arg)} 的路由信息`);
           return route;
         });
       } else {
-        log.error(`未检查到 ${chalk.red(arg)} 的路由信息
-           尝试如下命令启动：\n
-              ${chalk.green(r.map(i => `yarn start ${i}`).join('\n\t\t'))}
-        `);
+        log.error(`${
+          arg ? `未检查到 ${chalk.red(arg)} 的路由信息` : `未指定路由信息`
+        }
+             尝试如下命令启动：\n
+                ${chalk.green(r.map(i => `umi dev ${i}`).join('\n\t\t'))}
+          `);
         process.exit(0);
       }
     }
@@ -32,43 +34,14 @@ module.exports = function(api, options) {
     try {
       const routePath = path.resolve(paths.cwd, 'config/route.js');
       const routeFd = fs.statSync(routePath, { withFileTypes: true });
-      const route = require(routePath).default;
+      const route = require(routePath).default || [];
       api.modifyRoutes(() => {
         log.success(`已加载路由信息`);
         return route;
       });
     } catch (error) {
-      log.error(new Error('无路由信息'));
-      process.exit(1);
-    }
-  }
-
-  try {
-    const configPath = path.resolve(paths.cwd, 'config/config');
-    const configFd = fs.statSync(configPath, { withFileTypes: true });
-    const config = require(configPath).default;
-    api.modifyDefaultConfig(() => {
-      if (configFd.isDirectory() && findRoutes(configPath).includes(arg)) {
-        const locaConfig = require(path.resolve(configPath, arg)).default;
-        log.success(`已合并 ${arg} 的配置`);
-        return deeplyAssign({}, config, locaConfig);
-      } 
-      log.success(`已加载配置`);
-      return config
-    });
-  } catch (error) {
-
-    try {
-      const configPath = path.resolve(paths.cwd, 'config/config.js');
-      const configFd = fs.statSync(configPath, { withFileTypes: true });
-      const config = require(configPath).default;
-      api.modifyDefaultConfig(() => {
-        log.success(`已加载配置`);
-        return config;
-      });
-    } catch (error) {
-      log.error(new Error('无配置信息'));
-      process.exit(1);
+      log.error('无路由信息');
+      process.exit(0);
     }
   }
 };
